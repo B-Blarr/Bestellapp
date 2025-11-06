e2876c6ea191dab326426ecdc5da89c3396f8bc5
Benjamin Blarr <b.blarr@gmx.de>
2025-11-06 20:55:58 +0100

    Added JSON Array and small style changes

---
+++ scripts/db.js
@@ -0,0 +1,81 @@
+
+
+const dishes = [
+    {
+        "Neapolitanische Pizzen":
+        [
+            {
+                "name": "Margarita",
+                "price": 7.90,
+                "description": "mit Tomatensoße, Fior di Latte, Parmesan, Rucola, Basilikum und Kirschtomaten"
+            },
+            {
+                "name": "Salami",
+                "price": 8.90,
+                "description": "mit Tomatensoße, Rindersalami, Fior di Latte, Parmesan, Rucola, Basilikum und Kirschtomaten"
+            },
+            {
+                "name": "Schinken",
+                "price": 8.90,
+                "description": "mit Tomatensoße, Rinderschinken, Fior di Latte, Parmesan, Rucola, Basilikum und Kirschtomaten"
+            },
+            {
+                "name": "Thunfisch",
+                "price": 8.90,
+                "description": "mit Tomatensoße, Thunfisch, roten Zwiebeln, Fior di Latte, Parmesan, Rucola, Basilikum und Kirschtomaten"
+            },
+            {
+                "name": "Napoli",
+                "price": 10.90,
+                "description": "mit Tomatensoße, Fior di Latte, Parmesan, Sardellen, Kapern und Oliven"
+            },
+            {
+                "name": "Trüffel",
+                "price": 10.90,
+                "description": "mit Tomatensoße, Fior di Latte, Pilzen und Trüffelöl"
+            }
+        ],
+
+        "Desserts": [
+            {
+                "name": "Nutella-Pizza",
+                "price": 10.00,
+                "description": "mit Nutella, Bananen, Himbeeren und Schokoraspeln"
+            },
+            {
+                "name": "Tiramisu",
+                "price": 6.00,
+                "description": "Hausgemacht"
+            },
+            {
+                "name": "Panna Cotta",
+                "price": 5.00,
+                "description": "mit Früchten der Saison"
+            }
+        ],
+
+
+        "Getränke": [
+            {
+                "name": "Coca-Cola 0,33l (EINWEG)",
+                "price": 2.00,
+                "description": "0,33l, 6,06 €/1l, enthält Koffein (10mg/100ml)"
+            },
+            {
+                "name": "Fanta Orange 0,33l (EINWEG)",
+                "price": 2.00,
+                "description": "0,33l, 6,06 €/1l"
+            },
+            {
+                "name": "Sprite 0,33l (EINWEG)",
+                "price": 2.00,
+                "description": "0,33l, 6,06 €/1l"
+            },
+            {
+                "name": "Red Bull Energy Drink 0,25l (EINWEG)",
+                "price": 2.50,
+                "description": "0,25l, 10,00 €/1l, enthält Koffein (32mg/100ml)"
+            }
+        ]
+    }
+]
